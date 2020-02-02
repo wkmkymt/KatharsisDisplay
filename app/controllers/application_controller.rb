89947063137589bbedcf481ba68339d72124bd1f
wkmkymt <wkmkymt@gmail.com>
2020-02-03 06:50:00 +0900

    Add authentication in admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  # Security Error for Admin
+  rescue_from SecurityError do |exception|
+    redirect_to root_path, notice: '管理者画面へのアクセス権限がありません！'
+  end
+
+  protected
+
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.try(:admin?)
+  end
 end
