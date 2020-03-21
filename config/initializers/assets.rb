a46b6d4691aed2b318cf026836c5035c66bb423e
wkmkymt <wkmkymt@gmail.com>
2020-03-22 07:46:55 +0900

    Fix include path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( swiper/swiper.js )
-Rails.application.config.assets.precompile += %w( displays.js )
+Rails.application.config.assets.precompile += %w( swiper/swiper.js displays.js )
+Rails.application.config.assets.precompile += %w( swiper/swiper.css displays.scss )
