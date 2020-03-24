5fa2748191aac91ca37fc3653dc872666ac97d96
wkmkymt <wkmkymt@gmail.com>
2020-03-24 18:00:43 +0900

    Change table design

--- db/migrate/20200324075450_create_colors.rb
+++ db/migrate/20200324075450_create_colors.rb
@@ -1,7 +1,8 @@
 class CreateColors < ActiveRecord::Migration[5.2]
   def change
     create_table :colors do |t|
-      t.string :name
+      t.string :name, null: false
+      t.bigint :user_id, null: false
 
       t.timestamps
     end
--- db/migrate/20200324085722_create_photos.rb
+++ db/migrate/20200324085722_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-      t.binary :image
+      t.binary :image, null: false
+      t.bigint :user_id, null: false
 
       t.timestamps
     end
