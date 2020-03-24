9aac77b836b0a82cb2e7b004c4ff6f1cd21b2f06
Daichi <dai89e@gmail.com>
2020-03-25 02:35:48 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -97,7 +97,6 @@ ActiveRecord::Schema.define(version: 2020_03_24_085722) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0, null: false
     t.integer "point", default: 0, null: false
     t.integer "gender"
     t.date "birthday"
