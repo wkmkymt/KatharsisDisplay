7bca65b444e50c050ae3a1670cac6c02496a747e
wkmkymt <wkmkymt@gmail.com>
2020-03-24 19:17:24 +0900

    Add admin page of Color and Photo

---
+++ app/admin/photos.rb
@@ -0,0 +1,36 @@
+ActiveAdmin.register Photo do
+  # Permit
+  permit_params :image, :user_id
+
+  # Index
+  index do
+    selectable_column
+    column :id
+    column :image do |photo|
+      image_tag photo.image.url
+    end
+    column :user_id
+    actions
+  end
+
+  # Show
+  show do |photo|
+    attributes_table do
+      row :image do
+        image_tag photo.image.url
+      end
+      row :user_id
+    end
+  end
+
+  # Form
+  form do |f|
+    f.inputs do
+      f.input :image, :as => :file
+      f.input :user_id
+    end
+    f.actions
+  end
+
+
+end
