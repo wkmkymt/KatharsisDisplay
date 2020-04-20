5eba7156386f9f02a4b392865713aeef082eb0dc
Daichi <dai89e@gmail.com>
2020-04-20 17:37:24 +0900

    フォーム送信エラーのUI

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module App
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
   end
 end
