c6d33679084912c0713e195235925acfbbc4f4a6
Daichi <dai89e@gmail.com>
2020-04-27 02:32:56 +0900

    バックエンドあがき

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -27,4 +27,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
       resource.update_without_current_password(params)
     end
 
+  private
+
+    def crop
+      return
+      manipulate! do |img|
+        crop_x = params[:user][:x].to_i
+        crop_y = params[:user][:y].to_i
+        crop_w = params[:user][:width].to_i
+        crop_h = params[:user][:height].to_i
+        img.crop "#{crop_w}x#{crop_h}+#{crop_x}+#{crop_y}"
+        img = yield(img) if block_given?
+        img
+      end
+    end
+
 end
