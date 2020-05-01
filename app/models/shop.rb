196d29cf86220aadeffc45b7803d48beeb82c484
wkmkymt <wkmkymt@gmail.com>
2020-05-01 19:51:32 +0900

    Add relation between User and Shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,9 +1,13 @@
 class Shop < ApplicationRecord
+  # CheckinRecord -> User
   has_many :checkin_record, dependent: :delete_all
-  has_many :user, through: :checkin_record
+  has_many :checkined_user, through: :checkin_record, source: :user
+
+  # My User
+  has_many :user
 
   # Get Users Checked In
   def get_checkin_users
-    user.joins(:checkin_record).where('checkin_records.check_in', 'in').uniq
+    checkined_user.joins(:checkin_record).where('checkin_records.check_in', 'in').uniq
   end
 end
