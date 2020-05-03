c22d3bbef4283f850e13d74edec1a208326dc419
wkmkymt <wkmkymt@gmail.com>
2020-05-03 19:36:40 +0900

    Refactering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,22 +7,24 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    added_attrs = [ :name, :organization, :profile, :comment, :birthday, :gender, :personality, :color_id, :shop_id, :profimg, tag_ids: [] ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  end
+    def configure_permitted_parameters
+      added_attrs = [ :name, :organization, :profile, :comment, :birthday, :gender, :personality, :color_id, :shop_id, :profimg, tag_ids: [] ]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
 
-  # Security Error for Admin
-  rescue_from SecurityError do |exception|
-    redirect_to root_path, notice: '管理者画面へのアクセス権限がありません！'
-  end
+    # Authenticate Admin User
+    def authenticate_admin_user!
+      authenticate_user!
 
-  protected
+      unless current_user.has_role? :admin
+        raise SecurityError
+      end
+    end
 
-  def authenticate_admin_user!
-    authenticate_user!
-
-    raise SecurityError unless current_user.has_role? :admin
-  end
+    # Security Error for Admin
+    rescue_from SecurityError do |exception|
+      flash[:danger] = "管理者画面へのアクセス権限がありません！"
+      redirect_to root_path
+    end
 end
