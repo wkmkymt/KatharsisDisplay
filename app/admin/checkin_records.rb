b3aa4a1689deede42c550825a1c18aa3910ae70a
wkmkymt <wkmkymt@gmail.com>
2020-05-05 02:44:09 +0900

    Fix to be able to edit checkin redord

--- app/admin/checkin_records.rb
+++ app/admin/checkin_records.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register CheckinRecord do
   # Permit
   permit_params :user_id, :shop_id, :check_in
+
+  # Form
+  form do |f|
+    f.inputs do
+      f.input :user_id
+      f.input :shop_id
+      f.input :check_in, as: :select
+    end
+    f.actions
+  end
 end
