65256f4a331907bfc310c018fa0690fb47ed5a24
Daichi <dai89e@gmail.com>
2020-05-06 20:24:28 +0900

    パーソナリティの定数をリファクタリング
    再起動必要

---
+++ config/initializers/constants.rb
@@ -0,0 +1,47 @@
+module Constants
+    PERSONALITY_SELECT_OPTIONS = [
+      ['未診断の方は未回答', [
+        ['未回答', 'noanswer']
+      ]],
+      ['分析家', [
+        ['INTJ-A 建築家',   'INTJ_A'],
+        ['INTJ-T 建築家', 'INTJ_T'],
+        ['INTP-A 論理学者',    'INTP_A'],
+        ['INTP-T 論理学者',   'INTP_T'],
+        ['ENTJ-A 指揮官',   'ENTJ_A'],
+        ['ENTJ-T 指揮官',   'ENTJ_T'],
+        ['ENTP-A　討論者',   'ENTP_A'],
+        ['ENTP-T　討論者',   'ENTP_T'],
+      ]],
+      ['外交官', [
+        ['INFJ-A 提唱者',   'INFJ_A'],
+        ['INFJ-T 提唱者',   'INFJ_T'],
+        ['INFP-A 仲介者',   'INFP_A'],
+        ['INFP-T 仲介者',   'INFP_T'],
+        ['ENFJ-A 主人公',   'ENFJ_A'],
+        ['ENFJ-T 主人公',   'ENFJ_T'],
+        ['ENFP-A 広報運動家',   'ENFP_A'],
+        ['ENFP-T 広報運動家',   'ENFP_T'],
+      ]],
+      ['番人', [
+        ['ISTJ-A 管理者',   'ISTJ_A'],
+        ['ISTJ-T 管理者',   'ISTJ_T'],
+        ['ISFJ-A 擁護者',   'ISFJ_A'],
+        ['ISFJ-T 擁護者',   'ISFJ_T'],
+        ['ESTJ-A 幹部',   'ESTJ_A'],
+        ['ESTJ-T 幹部',   'ESTJ_T'],
+        ['ESFJ-A 領事官',   'ESFJ_A'],
+        ['ESFJ-T 領事官',   'ESFJ_T'],
+      ]],
+      ['探検家', [
+        ['ISTP-A 巨匠',   'ISTP_A'],
+        ['ISTP-T 巨匠',   'ISTP_T'],
+        ['ISFP-A 冒険家',   'ISFP_A'],
+        ['ISFP-T 冒険家',   'ISFP_T'],
+        ['ESTP-A 起業家',   'ESTP_A'],
+        ['ESTP-T 起業家',   'ESTP_T'],
+        ['ESFP-A エンターテイナー',   'ESFP_A'],
+        ['ESFP-T エンターテイナー',   'ESFP_T'],
+      ]]
+    ]
+end
