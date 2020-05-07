2c33c9c6907ee3b11f540e5d7c4723f8adb3a78f
wkmkymt <wkmkymt@gmail.com>
2020-05-08 03:47:25 +0900

    Add Category to admin page

--- app/admin/tags.rb
+++ app/admin/tags.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register Tag do
   # Permit
-  permit_params :name
+  permit_params :name, :category_id
+
+  # Index
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :category
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
